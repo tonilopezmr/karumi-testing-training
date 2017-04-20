7aae2108191c14905eb2b0c5bbaf4376462463b6
Antonio López Marín <tonilopezmr@gmail.com>
2017-04-20 18:57:36 +0200

    xd

--- app/src/main/java/com/tonilopezmr/karumitestingtraining/FizzBuzz.java
+++ app/src/main/java/com/tonilopezmr/karumitestingtraining/FizzBuzz.java
@@ -2,11 +2,17 @@ package com.tonilopezmr.karumitestingtraining;
 
 public class FizzBuzz {
 
+  public static final String FIZZ_BUZZ = "FizzBuzz";
+  public static final String FIZZ = "Fizz";
+  public static final String BUZZ = "Buzz";
+
   public static String fizzBuzz(int num) {
-    if (num % 3 == 0) {
-      return "Fizz";
+    if ((num % 3 == 0) && (num % 5 == 0)) {
+      return FIZZ_BUZZ;
+    } else if (num % 3 == 0) {
+      return FIZZ;
     } else if (num % 5 == 0) {
-      return "Buzz";
+      return BUZZ;
     } else {
       return String.valueOf(num);
     }
