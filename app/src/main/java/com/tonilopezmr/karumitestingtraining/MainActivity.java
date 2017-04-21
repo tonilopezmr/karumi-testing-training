709fb13fce998ac350f1255872cc35b8377a0360
Antonio López Marín <tonilopezmr@gmail.com>
2017-04-21 15:50:30 +0200

    Added UI behaviour

--- app/src/main/java/com/tonilopezmr/karumitestingtraining/MainActivity.java
+++ app/src/main/java/com/tonilopezmr/karumitestingtraining/MainActivity.java
@@ -1,7 +1,11 @@
 package com.tonilopezmr.karumitestingtraining;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,53 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    final SessionApiClient sessionApiClient = new SessionApiClient();
+
+    final EditText email = ((EditText) findViewById(R.id.emailEditText));
+    final EditText password = ((EditText) findViewById(R.id.passwordEditText));
+
+    final Button login = ((Button) findViewById(R.id.login));
+    Button logOut = ((Button) findViewById(R.id.logOut));
+
+    login.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        sessionApiClient.login(email.getText().toString(), password.getText().toString(), new SessionApiClient.Callback() {
+          @Override
+          public void onSuccess() {
+            email.setVisibility(View.INVISIBLE);
+            password.setVisibility(View.INVISIBLE);
+            login.setEnabled(false);
+            email.setText("");
+            password.setText("");
+          }
+
+          @Override
+          public void onError() {
+            Toast.makeText(MainActivity.this, "LOGIN ERROR", Toast.LENGTH_SHORT).show();
+          }
+        });
+      }
+    });
+
+    logOut.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        sessionApiClient.logout(new SessionApiClient.Callback() {
+          @Override
+          public void onSuccess() {
+            email.setVisibility(View.VISIBLE);
+            password.setVisibility(View.VISIBLE);
+            login.setEnabled(true);
+          }
+
+          @Override
+          public void onError() {
+            Toast.makeText(MainActivity.this, "LOGOUT ERROR", Toast.LENGTH_SHORT).show();
+          }
+        });
+      }
+    });
   }
 }
