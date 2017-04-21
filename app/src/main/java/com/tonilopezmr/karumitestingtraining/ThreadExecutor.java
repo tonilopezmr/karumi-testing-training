b966a9b974db854f36cabc62e49a5bba046cf2e3
Antonio López Marín <tonilopezmr@gmail.com>
2017-04-21 17:01:15 +0200

    Created thread executor and login test

--- app/src/main/java/com/tonilopezmr/karumitestingtraining/ThreadExecutor.java
+++ app/src/main/java/com/tonilopezmr/karumitestingtraining/ThreadExecutor.java
@@ -1,7 +1,9 @@
 package com.tonilopezmr.karumitestingtraining;
 
+import android.os.Handler;
+
 class ThreadExecutor {
-  public void post(Runnable runnable) {
-    new Thread(runnable).start();
+  public void post(final Runnable runnable) {
+    new Handler().postDelayed(runnable, 2000);
   }
 }
--- app/src/test/java/com/tonilopezmr/karumitestingtraining/SessionApiClientTest.java
+++ app/src/test/java/com/tonilopezmr/karumitestingtraining/SessionApiClientTest.java
@@ -1,5 +1,6 @@
 package com.tonilopezmr.karumitestingtraining;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
@@ -8,16 +9,77 @@ import static org.mockito.Mockito.verify;
 
 public class SessionApiClientTest {
 
+  private SessionApiClient sessionApiClient;
+
+  @Before
+  public void setUp() throws Exception {
+    sessionApiClient = new SessionApiClient(new FakeThreadExecutor());
+  }
+
+  @Test
+  public void
+  login_fails_if_the_pass_is_empty() throws Exception {
+    SessionApiClient.Callback mockCallback = mock(SessionApiClient.Callback.class);
+    sessionApiClient.login("user", "", mockCallback);
+
+
+    verify(mockCallback).onError();
+    verify(mockCallback, never()).onSuccess();
+  }
+
   @Test
   public void
   login_fails_if_the_user_is_empty() throws Exception {
-    SessionApiClient sessionApiClient = new SessionApiClient(new FakeThreadExecutor());
     SessionApiClient.Callback mockCallback = mock(SessionApiClient.Callback.class);
     sessionApiClient.login("", "pass", mockCallback);
 
 
     verify(mockCallback).onError();
     verify(mockCallback, never()).onSuccess();
+  }
+
+  @Test
+  public void
+  login_fails_if_the_user_and_pass_not_correct() throws Exception {
+    SessionApiClient.Callback mockCallback = mock(SessionApiClient.Callback.class);
+    sessionApiClient.login("user", "pass", mockCallback);
+
+
+    verify(mockCallback).onError();
+    verify(mockCallback, never()).onSuccess();
+  }
+
+  @Test
+  public void
+  login_fails_if_the_user_exists_but_pass_not_correct() throws Exception {
+    SessionApiClient.Callback mockCallback = mock(SessionApiClient.Callback.class);
+    sessionApiClient.login("toni", "233", mockCallback);
+
+
+    verify(mockCallback).onError();
+    verify(mockCallback, never()).onSuccess();
+  }
+
+  @Test
+  public void
+  login_fails_if_the_pass_correct_but_user_not_correct() throws Exception {
+    SessionApiClient.Callback mockCallback = mock(SessionApiClient.Callback.class);
+    sessionApiClient.login("user", "the3rocks", mockCallback);
+
+
+    verify(mockCallback).onError();
+    verify(mockCallback, never()).onSuccess();
+  }
+
+  @Test
+  public void
+  login_success_if_the_user_and_pass_correct() throws Exception {
+    SessionApiClient.Callback mockCallback = mock(SessionApiClient.Callback.class);
+    sessionApiClient.login("toni", "the3rocks", mockCallback);
+
+
+    verify(mockCallback).onSuccess();
+    verify(mockCallback, never()).onError();
   }
 
 }
